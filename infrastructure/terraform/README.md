91920c0dce575e92378e04c7210b7a386e76d9ce
Cesar Rodriguez <cesar@appcd.com>
2025-12-17 17:40:03 +0000

    Commit by Cesar Rodriguez

---
+++ infrastructure/terraform/README.md
@@ -0,0 +1,3 @@
+# README
+This is a readme file for IaC generated with StackGen.
+You can modify your appStack -> [here](http://appcd.local/appstacks/5ee0b035-0dbd-4c53-95ac-f7561a4a5dd3)
---
+++ infrastructure/terraform/outputs.tf
@@ -0,0 +1,53 @@
+output "aws_ecs_stackgen_148c832d-5997-4721-9cb7-d00381c60844_arn" {
+  value     = module.stackgen_148c832d-5997-4721-9cb7-d00381c60844.arn
+  sensitive = false
+}
+
+output "aws_ecs_cluster_stackgen_25703056-2b89-4d7e-940d-3b8879a107f5_arn" {
+  value     = module.stackgen_25703056-2b89-4d7e-940d-3b8879a107f5.arn
+  sensitive = false
+}
+
+output "aws_ecs_cluster_stackgen_25703056-2b89-4d7e-940d-3b8879a107f5_name" {
+  value     = module.stackgen_25703056-2b89-4d7e-940d-3b8879a107f5.name
+  sensitive = false
+}
+
+output "aws_iam_role_stackgen_c9147ba6-dd2c-44ae-9367-295a7a77b2c9_arn" {
+  value     = module.stackgen_c9147ba6-dd2c-44ae-9367-295a7a77b2c9.arn
+  sensitive = false
+}
+
+output "aws_iam_role_stackgen_c9147ba6-dd2c-44ae-9367-295a7a77b2c9_name" {
+  value     = module.stackgen_c9147ba6-dd2c-44ae-9367-295a7a77b2c9.name
+  sensitive = false
+}
+
+output "aws_s3_stackgen_84b271c1-8185-4192-a0e2-8bcc7369838a_arn" {
+  value     = module.stackgen_84b271c1-8185-4192-a0e2-8bcc7369838a.arn
+  sensitive = false
+}
+
+output "aws_s3_stackgen_84b271c1-8185-4192-a0e2-8bcc7369838a_bucket_name" {
+  value     = module.stackgen_84b271c1-8185-4192-a0e2-8bcc7369838a.bucket_name
+  sensitive = false
+}
+
+output "aws_s3_stackgen_84b271c1-8185-4192-a0e2-8bcc7369838a_bucket_website_endpoint" {
+  value     = module.stackgen_84b271c1-8185-4192-a0e2-8bcc7369838a.bucket_website_endpoint
+  sensitive = false
+}
+
+output "aws_s3_stackgen_84b271c1-8185-4192-a0e2-8bcc7369838a_kms_arn" {
+  value     = module.stackgen_84b271c1-8185-4192-a0e2-8bcc7369838a.kms_arn
+  sensitive = false
+}
+
+output "aws_sns_stackgen_26d13ef2-e32f-4330-9031-23ff60322c3d_arn" {
+  value     = module.stackgen_26d13ef2-e32f-4330-9031-23ff60322c3d.arn
+  sensitive = false
+}
+
+
+################################################################################
+
